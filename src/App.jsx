9fec6d2ff4d8c93ac337f51c65cd9f9b3a1bc7d0
hashmi7917 <hmuqtadeer@gmail.com>
2023-12-23 20:51:13 +0530

    mob version done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
+
+import Logo from './assets/images/logo.svg';
+import HambMenu from './assets/images/icon-menu.svg';
+import HeroMobile from './assets/images/image-hero-mobile.png';
+import Databiz from './assets/images/client-databiz.svg';
+import Audiophile from './assets/images/client-audiophile.svg';
+import Meet from './assets/images/client-meet.svg';
+import Maker from './assets/images/client-maker.svg';
 
 function App() {
   return (
-    <div className='max-w-xs'>
-      <h1>Tailwind Starter React+Vite+React</h1>
+    <div className="max-w-sm mx-auto shadow-md pb-20">
+      <div className="flex flex-col justify-evenly items-center">
+        <nav className="w-full flex justify-between items-center py-4 px-4">
+          <img src={Logo} alt="logo" />
+          <img src={HambMenu} alt="mobile menu" />
+        </nav>
+        <header className="flex flex-col items-center py-2">
+          <img src={HeroMobile} alt="hero" />
+          <div className="flex flex-col justify-around items-center h-60 pt-8">
+            <h1 className="text-4xl font-medium text-neutral-AlmostBlack">
+              Make remote work
+            </h1>
+            <p className="text-center text-sm text-neutral-MediumGray leading-relaxed mx-auto w-10/12 px-2">
+              Get your team in sync, no matter your location. Streamline
+              processes, create team rituals, and watch productivity soar.
+            </p>
+            <button
+              type="button"
+              class="text-neutral-AlmostWhite bg-neutral-AlmostBlack hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-normal rounded-xl text-sm px-6 py-2.5 dark:bg-neutral-AlmostBlack dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+            >
+              Learn more
+            </button>
+          </div>
+          <div className="w-full flex justify-around items-center pt-8">
+            <img className="h-3" src={Databiz} alt="databiz" />
+            <img className="h-5" src={Audiophile} alt="audiophile" />
+            <img className="h-3" src={Meet} alt="meet" />
+            <img className="h-5" src={Maker} alt="maker" />
+          </div>
+        </header>
+      </div>
     </div>
-  )
+  );
 }
-
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Epilogue: ['Epilogue', 'sans-serif'],
+      },
+      fontWeight: {
+        normal: '500',
+        medium: '700',
+      },
+      fontSize: {
+        md: '16px',
+        xxs: '12px',
+      },
+      colors: {
+        neutral: {
+          AlmostWhite: 'hsl(0, 0%, 98%)',
+          MediumGray: 'hsl(0, 0%, 41%)',
+          AlmostBlack: 'hsl(0, 0%, 8%)',
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
