e0d3515cdaef82121e79f73c0c67791c51ca55cc
hashmi7917 <hmuqtadeer@gmail.com>
2023-12-24 13:12:26 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import ReactDOM from 'react-dom';
 import './App.css';
 
+// importing image assets
 import Logo from './assets/images/logo.svg';
 import HambMenu from './assets/images/icon-menu.svg';
+import Closemenu from './assets/images/icon-close-menu.svg';
 import HeroMobile from './assets/images/image-hero-mobile.png';
 import Databiz from './assets/images/client-databiz.svg';
 import Audiophile from './assets/images/client-audiophile.svg';
@@ -9,12 +12,47 @@ import Meet from './assets/images/client-meet.svg';
 import Maker from './assets/images/client-maker.svg';
 
 function App() {
+  function openNav() {
+    let sidemenu = document.getElementById('sidemenu');
+    ReactDOM.findDOMNode(sidemenu).style.display = 'block';
+  }
+  function closeNav() {
+    let sidemenu = document.getElementById('sidemenu');
+    ReactDOM.findDOMNode(sidemenu).style.display = 'none';
+  }
+
   return (
     <div className="max-w-sm mx-auto shadow-md pb-20">
       <div className="flex flex-col justify-evenly items-center">
         <nav className="w-full flex justify-between items-center py-4 px-4">
           <img src={Logo} alt="logo" />
-          <img src={HambMenu} alt="mobile menu" />
+          <img
+            onClick={openNav}
+            className="cursor-pointer"
+            src={HambMenu}
+            alt="mobile menu"
+          />
+          <div
+            id="sidemenu"
+            className="hidden bg-neutral-AlmostWhite absolute top-0 right-14 px-20 h-screen shadow-xl"
+          >
+            <div className="flex flex-col py-20">
+              <img
+                className="w-5 self-end fixed right-20 top-10 transition-all ease-in-out"
+                onClick={closeNav}
+                src={Closemenu}
+                alt="close menu"
+              />
+
+              <ul className="w-full flex flex-col justify-around items-center mx-auto h-48 transition-all ease-in-out duration-75">
+                <li className="cursor-pointer">Home</li>
+                <li className="cursor-pointer">About</li>
+                <li className="cursor-pointer">Contact</li>
+                <li className="cursor-pointer">Blog</li>
+                <li className="cursor-pointer">Careers</li>
+              </ul>
+            </div>
+          </div>
         </nav>
         <header className="flex flex-col items-center py-2">
           <img src={HeroMobile} alt="hero" />
@@ -33,7 +71,7 @@ function App() {
               Learn more
             </button>
           </div>
-          <div className="w-full flex justify-around items-center pt-8">
+          <div className="w-full flex justify-around items-center pt-10">
             <img className="h-3" src={Databiz} alt="databiz" />
             <img className="h-5" src={Audiophile} alt="audiophile" />
             <img className="h-3" src={Meet} alt="meet" />
